b8b694a057b521e05b5d6bc14575f56f822ea342
Shoaib Bhimani <shoaibbhimani127@hotmail.com>
2017-04-16 01:10:21 +0530

    nested Routes completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <h1>Home</h1>
+)
+
+const About = () => (
+  <h1>About</h1>
+)
+
+const Links = () => (
+ <div className="list-group">
+    <NavLink className="list-group-item" exact activeClassName="active" to="/" >Home</NavLink>
+   <NavLink className="list-group-item" activeClassName="active" to="/about" >About</NavLink>
+   <NavLink className="list-group-item" activeClassName="active" to="/content" >Content</NavLink>
+  </div>
+)
+
+const Content = () => (
+  <div>
+     <NavLink className="list-group-item"  activeClassName="active" to="/content/city">City</NavLink>
+     <NavLink className="list-group-item"  activeClassName="active" to="/content/sports">Sports</NavLink>
+     <Route  path="/content/:contentName" component={ContentDetails} />
+  </div>
+)
+
+const ContentDetails = (props) => (
+  <div>
+  { props.match.params.contentName ? 
+    <div>
+    <img src={'http://lorempixel.com/400/200/'+props.match.params.contentName+'/1/'} /> 
+    </div> 
+    : 
+    null
+   }
+   </div>
+)
+
+const App = () => (
+  <Router>
+  <div className="row">
+    <section className="col-sm-4"> 
+      <Links />
+    </section>
+    
+    <section  className="col-sm-8">
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+       <Route path="/content" component={Content} />
+    </section>
+    
+    </div>
+  </Router>
+)
 
 export default App;
